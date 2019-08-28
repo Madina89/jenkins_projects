20b4fd46a44906a6053b8ce2ca6842c46b5de00a
Madina89 <madinalinux89@gmail.com>
2019-08-28 18:53:28 -0500

    added pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+node{
+    stage("Update jenkins"){
+         properties([parameters([string(defaultValue: '63.35.199.158', description: 'Please provide IP', name: 'Environment ', trim: true)])])
+         sh "echo Paramater added"
+    }
+    stage("Install git"){
+        sh "ssh ec2-user@${Enviroment} sudo yum install git -y"
+    }
+    stage("Pull Repo"){
+      sh "ssh ec2-user@${Enviroment} git clone https://github.com/Madina89/flask-examples.git"
+    }
+    stage("Install Reguirements"){
+       // sh "virualenv /tmp/venv"
+        //sh ". /tmpvenv/bin/activate"
+        sh "echo Hello"
+    }
+    stage("Pip Install"){
+        sh "ssh ec2-user@${Enviroment} pip install -r ~/flsk-examples/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh ec2-user@${Enviroment} python  ~/flask-examples/01-hello-world/hello.py"
+    }
+}
